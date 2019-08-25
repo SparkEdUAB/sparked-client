d8614b21f9fb072f89ec7c2a5acb5cb5ab36d83d
OlivierJM <manolivier93@gmail.com>
2019-08-25 15:19:07 +0200

    added basic examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './Pages/Home';
 
-import './App.css';
+const isDark = true;
+if (isDark) {
+  require('@elastic/eui/dist/eui_theme_dark.css');
+} else {
+  require('@elastic/eui/dist/eui_theme_light.css');
+}
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Home />;
 };
 
 export default App;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+  EuiPageContentHeader,
+  EuiPageContentHeaderSection,
+  EuiTitle
+} from '@elastic/eui';
+
+const Home = () => (
+  <EuiPage>
+    <EuiPageBody>
+      <EuiPageContent verticalPosition="center" horizontalPosition="center">
+        <EuiPageContentHeader>
+          <EuiPageContentHeaderSection>
+            <EuiTitle>
+              <h2>Content title</h2>
+            </EuiTitle>
+          </EuiPageContentHeaderSection>
+        </EuiPageContentHeader>
+        <EuiPageContentBody>Content body</EuiPageContentBody>
+      </EuiPageContent>
+    </EuiPageBody>
+  </EuiPage>
+);
+export default Home;
