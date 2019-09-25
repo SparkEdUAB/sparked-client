fa350c88bd659ecadadcbeecb8be3d49bc3c9e81
Olivier <olivier@thezig.io>
2019-09-25 23:12:04 +0200

    added the sidebar navigation for the dashboard

--- src/core/routes/Routes.js
+++ src/core/routes/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Register from '../component/Auth/Register'
+import SimpleSidebar from '../../admin/component/Sidebar'
 
 
 function Routes(){
     return(
-        <Register />
+        // <Register />
+        <SimpleSidebar />
     )
 }
 export default Routes
