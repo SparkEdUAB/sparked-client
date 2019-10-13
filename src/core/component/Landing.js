baf3a4a81c8625e2987b55536e0f2d316aeff6de
Olivier <olivier@thezig.io>
2019-10-13 12:53:43 +0200

    checking if logged in user is client or admin

--- src/core/component/Landing.js
+++ src/core/component/Landing.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/landing.css'
+import { useQuery } from '@apollo/react-hooks'
+import USER_INFO from '../queries/userInfoQuery'
 
+// Todo: Handle the errors and loading properly
 function Landing() {
+  const { loading, data, error } = useQuery(USER_INFO)
+  if (loading) {
+    return 'loading'
+  }
+  if (error) {
+    return 'error'
+  }
   return (
     <>
       <div className="bg"></div>
       <div className="flex-container">
-        <Link to="/admin/overview">Dashboard</Link>
+        {data && data.me.role === 'admin' ? (
+          <Link to="/admin/overview">Dashboard</Link>
+        ) : (
+          <Link to="/client/courses">Courses</Link>
+        )}
       </div>
     </>
   )
