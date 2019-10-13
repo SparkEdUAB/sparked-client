868ee50381deab59429b88e3e2983c36f6948d56
Olivier <olivier@thezig.io>
2019-10-13 13:00:44 +0200

    removed the check on the landing page

--- src/core/component/Landing.js
+++ src/core/component/Landing.js
@@ -1,27 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/landing.css'
-import { useQuery } from '@apollo/react-hooks'
-import USER_INFO from '../queries/userInfoQuery'
+// import { useQuery } from '@apollo/react-hooks'
+// import USER_INFO from '../queries/userInfoQuery'
 
 // Todo: Handle the errors and loading properly
 function Landing() {
-  const { loading, data, error } = useQuery(USER_INFO)
-  if (loading) {
-    return 'loading'
-  }
-  if (error) {
-    return 'error'
-  }
+  //   const { loading, data, error } = useQuery(USER_INFO)
+  //   if (loading) {
+  //     return 'loading'
+  //   }
   return (
     <>
       <div className="bg"></div>
       <div className="flex-container">
-        {data && data.me.role === 'admin' ? (
-          <Link to="/admin/overview">Dashboard</Link>
-        ) : (
-          <Link to="/client/courses">Courses</Link>
-        )}
+        <h1>Welcome To SparkEd</h1>
+        <Link to="/client/courses">Courses</Link>
       </div>
     </>
   )
