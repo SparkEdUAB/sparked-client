4d40c97e5b1a065baad42de6a9a0595b1c8279b5
Olivier <olivier@thezig.io>
2019-10-13 18:55:21 +0200

    cleaned the landing page

--- src/core/component/Landing.js
+++ src/core/component/Landing.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/landing.css'
-// import { useQuery } from '@apollo/react-hooks'
-// import USER_INFO from '../queries/userInfoQuery'
+import Button from 'react-rainbow-components/components/Button'
+import { Row, Col } from 'react-flexbox-grid'
 
-// Todo: Handle the errors and loading properly
 function Landing() {
-  //   const { loading, data, error } = useQuery(USER_INFO)
-  //   if (loading) {
-  //     return 'loading'
-  //   }
   return (
     <>
       <div className="bg"></div>
       <div className="flex-container">
-        <h1>Welcome To SparkEd</h1>
-        <Link to="/client/courses">Courses</Link>
+        <h1
+          style={{
+            fontSize: '4em',
+            color: 'teal',
+            fontWeight: 'bold',
+          }}
+        >
+          Welcome To SparkEd V3
+        </h1>
+        <Row>
+          <Col xs={12}>
+            <Row center="xs">
+              <Col xs={6}>
+                <Button variant="base">
+                  <Link
+                    style={{
+                      fontWeight: 'bold',
+                      textDecoration: 'none',
+                    }}
+                    to="/login"
+                  >
+                    Get Started
+                  </Link>
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </div>
     </>
   )
