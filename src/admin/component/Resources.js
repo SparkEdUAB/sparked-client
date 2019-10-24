182995da513b7964f79576423a5f42b94adaf624
OlivierJM <manolivier93@gmail.com>
2019-10-24 18:44:43 +0200

    fixed file uploads

---
+++ src/admin/component/Resources.js
@@ -0,0 +1,177 @@
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/react-hooks'
+import { Link } from 'react-router-dom'
+import {
+  Pagination,
+  Spinner,
+  Table,
+  Column,
+  MenuItem,
+  Badge,
+  Modal,
+  Input,
+  Button,
+} from 'react-rainbow-components'
+import { IoIosAdd, IoIosRemoveCircleOutline } from 'react-icons/io'
+import GET_TOPICS, { CREATE_TOPIC, DELETE_TOPIC } from '../queries/topics.query'
+import ErrorPage from '../../core/component/utils/ErrorPage'
+import '../styles/styles.css'
+import { renderPaginatedData } from '../../core/component/utils/utils'
+
+const badgeStyles = { color: '#1de9b6' }
+
+const StatusBadge = ({ value }) => (
+  <Badge label={value} variant="lightest" style={badgeStyles} />
+)
+function TopicsList({ match }) {
+  const unitId = match.params.id
+
+  const courseId = '5db0b29b4cd2038881beab0e'
+  const { loading, data, error } = useQuery(GET_TOPICS, {
+    variables: { unitId },
+  })
+  const [createtopic] = useMutation(CREATE_TOPIC)
+  const [deletetopic] = useMutation(DELETE_TOPIC)
+  const [activePage, setActivePage] = useState(1)
+  const [isOpen, setModal] = useState(false)
+  const [name, setName] = useState('')
+  // const [topicIds, setTopicIds] = useState([])
+  const itemsPerPage = 10
+  let topicIds = []
+
+  function handleOnChange(event, page) {
+    setActivePage(page)
+  }
+
+  if (loading) {
+    return (
+      <div className="rainbow-p-vertical_xx-large">
+        <div className="rainbow-position_relative rainbow-m-vertical_xx-large rainbow-p-vertical_xx-large">
+          <Spinner size="large" />
+        </div>
+      </div>
+    )
+  }
+
+  if (error) return <ErrorPage />
+
+  function handleOnClick(data) {
+    setModal(true)
+    setName(data.name)
+  }
+  function handleOnClose() {
+    setModal(false)
+  }
+  function handleOnDelete() {
+    if (!topicIds.length) {
+      return null
+    }
+    deletetopic({
+      variables: { ids: topicIds },
+      refetchQueries: [{ query: GET_TOPICS }],
+    })
+  }
+
+  function handleCreateTopic() {
+    createtopic({
+      variables: { name, unitId, courseId },
+      refetchQueries: [{ query: GET_TOPICS, variables: { unitId, courseId } }],
+    }).then(() => {
+      setName('')
+      setModal(false)
+    })
+  }
+
+  return (
+    <div className="rainbow-p-bottom_xx-large">
+      <Modal id="modal-1" isOpen={isOpen} onRequestClose={handleOnClose}>
+        <Input
+          label="Topic"
+          placeholder="Enter your name"
+          type="text"
+          className="rainbow-p-around_medium"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <Button
+          isLoading={false}
+          label={name.length ? 'update' : 'add'}
+          variant="outline-brand"
+          className="rainbow-m-around_medium"
+          onClick={handleCreateTopic}
+        />
+      </Modal>
+      <div>
+        <Button
+          variant="neutral"
+          className="rainbow-m-around_medium"
+          onClick={() => setModal(true)}
+        >
+          New
+          <IoIosAdd size={'2em'} />
+        </Button>
+        <Button
+          variant="neutral"
+          className="rainbow-m-around_medium"
+          onClick={handleOnDelete}
+        >
+          Delete
+          <IoIosRemoveCircleOutline size={'2em'} />
+        </Button>
+
+        <Table
+          keyField="_id"
+          isLoading={loading}
+          data={renderPaginatedData(
+            data.getTopicsByUnitId,
+            activePage,
+            itemsPerPage
+          )}
+          showCheckboxColumn
+          maxRowSelection={itemsPerPage}
+          selectedRows={['1234qwerty', '1234zxcvbn']}
+          onRowSelection={data => {
+            // To avoid an overflow in states, directly mutate the ids
+            const ids = data.map(unit => unit._id)
+            topicIds = ids
+          }}
+        >
+          <Column
+            header="Name"
+            field="name"
+            component={({ value, row }) => (
+              <Link
+                className="react-rainbow-admin-users_user-id-cell-container"
+                to={`/admin/topic/${row._id}`}
+              >
+                <div className="react-rainbow-admin-users_user-id-cell rainbow-color_brand">
+                  {value}
+                </div>
+              </Link>
+            )}
+          />
+          <Column
+            header="created At"
+            field="createdAt"
+            component={StatusBadge}
+          />
+          <Column header="created By" field="createdBy" />
+          <Column type="action">
+            <MenuItem label="Edit" onClick={(e, data) => handleOnClick(data)} />
+            <MenuItem label="Delete" onClick={handleOnDelete} />
+          </Column>
+        </Table>
+        {(data.getTopicsByUnitId.length < 10) &
+        (
+          <Pagination
+            className="rainbow-m_auto"
+            pages={data.getTopicsByUnitId.length / itemsPerPage}
+            activePage={activePage}
+            onChange={handleOnChange}
+          />
+        ) || null}
+      </div>
+    </div>
+  )
+}
+export default TopicsList
--- src/client/component/Uploads.js
+++ src/client/component/Uploads.js
@@ -1,6 +1,7 @@
 import React, { useState, Fragment } from 'react'
 import { gql } from 'apollo-boost'
 import { useMutation } from '@apollo/react-hooks'
+import '../../admin/styles/resources.css'
 
 const UPLOAD_FILE = gql`
   mutation($file: Upload!) {
@@ -11,13 +12,12 @@ const UPLOAD_FILE = gql`
 `
 
 export default function FileUploads() {
-  const [fileState, setState] = useState(null)
+  const [fileState, setState] = useState([])
+  const [fileSize, setFileSize] = useState(0)
   const [singleUpload] = useMutation(UPLOAD_FILE)
 
   function handleFileUpload(e) {
     e.preventDefault()
-    // const { files } = e.target
-    // console.log(files[0])
 
     singleUpload({
       variables: { file: fileState[0] },
@@ -25,10 +25,50 @@ export default function FileUploads() {
       .then(data => console.log(data))
       .catch(error => console.log(error))
   }
+
+  function handleFileChange(e) {
+    const files = e.target.files
+    setState(files)
+    setFileSize(convertUploadSize(getFileSizes(files)))
+  }
   return (
     <Fragment>
-      <input type="file" required onChange={e => setState(e.target.files)} />
-      <button onClick={handleFileUpload}>submit</button>
+      {/* <input type="file" required onChange={e => setState(e.target.files)} />
+       */}
+      <div className="file-input">
+        <input
+          multiple
+          type="file"
+          name="files[]"
+          onChange={handleFileChange}
+        />
+        <span className="button">Choose</span>
+        <span className="label" data-js-label>
+          {fileSize ? `Total size: ${fileSize}` : 'No file selected'}
+        </span>
+      </div>
+      <button onClick={handleFileUpload}>Upload</button>
     </Fragment>
   )
+}
+
+function getFileSizes(files) {
+  const all_size = []
+  for (let index = 0; index < files.length; index++) {
+    const file = files[index]
+    all_size.push(file.size)
+  }
+  const sum = all_size.reduce((a, b) => a + b, 0) // adds all values in the all_sizes array
+  return sum
+}
+export function convertUploadSize(bytes) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  if (bytes === 0) {
+    return 'n/a'
+  }
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
+  if (i === 0) {
+    return `${bytes} ${sizes[i]})`
+  }
+  return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`
 }
